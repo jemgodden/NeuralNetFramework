baa6248f7a9b15982bb4eba2be81fc2a3a96262c
jemgodden <jemgodden@gmail.com>
2025-10-29 17:11:37 +0000

    Adding more activation functions

--- include/nnf/network/activations.h
+++ include/nnf/network/activations.h
@@ -1,6 +1,10 @@
 #pragma once
 
 
-double square(double value);
+static double square(double value);
 
-double sigmoid(double value);
+static double sigmoid(double value);
+
+static double Relu(double value);
+
+static double Tanh(double value);
--- src/network/activations.cpp
+++ src/network/activations.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <algorithm>
 
 #include "../../include/nnf/network/activations.h"
 
@@ -9,4 +10,12 @@ double square(double const value) {
 
 double sigmoid(const double value) {
     return 1.0 / (1.0 + exp(-1.0 * value));
+};
+
+double ReLU(const double value) {
+    return std::max(0.0, value);
+};
+
+double Tanh(const double value) {
+    return (exp(value) - exp(-value)) / (exp(value) + exp(-value));
 };
