b9a5f3bd56d5b06be276776478623cda21664b3b
jemgodden <jemgodden@gmail.com>
2025-10-29 21:11:38 +0000

    Adding activation derivatives

--- include/nnf/network/activations.h
+++ include/nnf/network/activations.h
@@ -3,8 +3,16 @@
 
 static double square(double value);
 
+static double squareDerivative(double value);
+
 static double sigmoid(double value);
+
+static double sigmoidDerivative(double value);
 
 static double Relu(double value);
 
+static double ReluDerivative(double value);
+
 static double Tanh(double value);
+
+static double TanhDerivative(double value);
--- src/network/activations.cpp
+++ src/network/activations.cpp
@@ -8,14 +8,33 @@ double square(double const value) {
     return std::pow(value, 2.0);
 };
 
+double squareDerivative(double const value) {
+    return 2.0 * value;
+};
+
 double sigmoid(const double value) {
     return 1.0 / (1.0 + exp(-1.0 * value));
+};
+
+double sigmoidDerivative(const double value) {
+    return value / (1.0 - value);
 };
 
 double ReLU(const double value) {
     return std::max(0.0, value);
 };
 
+double ReLUDerivative(const double value) {
+    if (value <= 0.0) {
+        return 0.0;
+    }
+    return 1.0;
+};
+
 double Tanh(const double value) {
     return (exp(value) - exp(-value)) / (exp(value) + exp(-value));
+};
+
+double TanhDerivative(const double value) {
+    return 1.0 - std::pow(value, 2.0);
 };
