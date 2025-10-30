2111b331d06d57458a26f0aaa9d58987dfebc069
jemgodden <jemgodden@gmail.com>
2025-10-30 20:51:56 +0000

    Adding categorical cross entropy loss for backpropagation

--- include/nnf/network/costs.h
+++ include/nnf/network/costs.h
@@ -3,8 +3,12 @@
 #include "../matrix/matrix.h"
 
 
-void checkCostFunctionInputs(const Matrix* actual, const Matrix* predicted);
+void checkContinuousCostFunctionInputs(const Matrix* actual, const Matrix* predicted);
 
-static double meanSquaredError(const Matrix* actual, const Matrix* predicted);
+double meanSquaredError(const Matrix* actual, const Matrix* predicted);
 
-static double crossEntropyLoss(const Matrix* actual, const Matrix* predicted);
+double continuousCrossEntropyLoss(const Matrix* actual, const Matrix* predicted);
+
+double categoricalCrossEntropyLoss(const Matrix* actual, const Matrix* predicted);
+
+Matrix* categoricalCrossEntropyLossDerivative(const Matrix* actual, const Matrix* predicted);
--- src/network/costs.cpp
+++ src/network/costs.cpp
@@ -6,7 +6,7 @@
 #include "../../include/nnf/utils/exception.h"
 
 
-void checkCostFunctionInputs(const Matrix* actual, const Matrix* predicted) {
+void checkContinuousCostFunctionInputs(const Matrix* actual, const Matrix* predicted) {
     if (actual->cols() > 1 || predicted->cols() > 1) {
         throw IllegalMatrixOperation("Cost functions can only be calculated on two column vectors.");
     }
@@ -17,7 +17,7 @@ void checkCostFunctionInputs(const Matrix* actual, const Matrix* predicted) {
 
 double meanSquaredError(const Matrix* actual, const Matrix* predicted) {
     try {
-        checkCostFunctionInputs(actual, predicted);
+        checkContinuousCostFunctionInputs(actual, predicted);
     }
     catch (std::exception& e) {
         std::cerr << e.what() << std::endl;
@@ -31,9 +31,9 @@ double meanSquaredError(const Matrix* actual, const Matrix* predicted) {
     return 0.5 * curSum;
 };
 
-double crossEntropyLoss(const Matrix* actual, const Matrix* predicted) {
+double continuousCrossEntropyLoss(const Matrix* actual, const Matrix* predicted) {
     try {
-        checkCostFunctionInputs(actual, predicted);
+        checkContinuousCostFunctionInputs(actual, predicted);
     }
     catch (std::exception& e) {
         std::cerr << e.what() << std::endl;
@@ -45,4 +45,42 @@ double crossEntropyLoss(const Matrix* actual, const Matrix* predicted) {
         curSum += actual->get(i, 1) * std::log(predicted->get(i, 1));
     }
     return -curSum;
+};
+
+double categoricalCrossEntropyLoss(const Matrix* actual, const Matrix* predicted) {
+    try {
+        if (actual->rows() != predicted->rows()) {
+            throw IllegalMatrixOperation("Cost functions can only be calculated on two column vectors.matrices with the same number of inputs.");
+        }
+    }
+    catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        throw;
+    }
+
+    double curSum = 0;
+    for (int i=0; i<actual->rows(); i++) {
+        const int actualIndex = static_cast<int>(actual->get(i, 0)) - 1;
+        curSum += std::log(predicted->get(i, actualIndex));
+    }
+    return -curSum;
+};
+
+Matrix* categoricalCrossEntropyLossDerivative(const Matrix* actual, const Matrix* predicted) {
+    try {
+        if (actual->rows() != predicted->rows()) {
+            throw IllegalMatrixOperation("Cost functions can only be calculated on two column vectors/matrices with the same number of inputs.");
+        }
+    }
+    catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        throw;
+    }
+
+    Matrix* output = new Matrix(actual->rows(), 1);
+    for (int i=0; i<actual->rows(); i++) {
+        const int actualIndex = static_cast<int>(actual->get(i, 0)) - 1;
+        output->set(i, 1, -1.0 / predicted->get(i, actualIndex));
+    }
+    return output;
 };
